6444e3faff9ac4b3f14ae0555feb34f2c9c3e8fe
hansheng <hansheng1224@gmail.com>
2022-11-14 11:53:46 +0800

    新增刪除資料

--- index.php
+++ index.php
@@ -12,22 +12,43 @@
 <body>
     <h1>學生管理系統</h1>
     <?php
+    $dsn = "mysql:host=localhost;charset=utf8;dbname=school";
+    $pdo = new PDO($dsn, 'root', '');
+
+    $sql = "SELECT * FROM `students` LIMIT 20";
+
+    $rows = $pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
 
 
-    $dsn="mysql:host=localhost;charset=utf8;dbname=school";
-    $pdo=new PDO($dsn,'root','');
-
-    $sql="SELECT * FROM `students` LIMIT 5";
-
-
-    $rows=$pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
-
-
-    echo "<pre>";
-    print_r($rows);
-    echo "</pre>"
-
+    // echo "<pre>";
+    // print_r($rows);
+    // echo "</pre>"
     ?>
+    
+
+    <table class='list-students'>
+    <tr>
+        <td>學號</td>
+        <td>姓名</td>
+        <td>生日</td>
+        <td>畢業國中</td>
+        <td>年齡</td>
+    </tr>
+       <?php
+        foreach ($rows as $row) {
+$age=round((strtotime('now')-strtotime($row['birthday']))/(60*60*24*365),1);
+
+            echo "<tr>";
+            echo "<td>{$row['school_num']}</td>";
+            echo "<td>{$row['name']}</td>";
+            echo "<td>{$row['birthday']}</td>";
+            echo "<td>{$row['graduate_at']}</td>";
+            echo "<td>$age</td>";
+            echo "</tr>";
+        }
+
+        ?>
+    </table>
 </body>
 
 </html>
